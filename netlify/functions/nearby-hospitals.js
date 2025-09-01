89d3ac5af259d2522dec26293bdd858a29a4c2d4
Henry <Henries90@gmail.com>
2025-09-01 23:22:54 +0100

    Trying to use netlify functions

--- netlify/functions/nearby-hospitals.js
+++ netlify/functions/nearby-hospitals.js
@@ -3,10 +3,11 @@ export async function handler(event) {
 
   // eslint-disable-next-line no-undef
   const url = `https://api.geoapify.com/v2/places?categories=healthcare.hospital&filter=circle:${lon},${lat},5000&apiKey=${process.env.GEOAPIFY_KEY}`;
+  console.log(url);
 
   const res = await fetch(url); // no import needed
   const data = await res.json();
-
+  console.log(data);
   return {
     statusCode: 200,
     body: JSON.stringify(data),
