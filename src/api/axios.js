4051f3fe0b7c2fdb386b6fde88d2543001db7061
Henry <Henries90@gmail.com>
2025-09-01 22:18:37 +0100

    Changed baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://homedoc-backend-69l2.onrender.com",
   withCredentials: true,
 });
